a049305999bad6dee5b58941c268a2d154984980
Yanik <emmenegger@yanik.pro>
2024-12-09 15:43:02 +0100

    init

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// /src/components/index.ts
+
+import ZoomControls from './ZoomControls';
+import Tooltip from './Tooltip';
+import CountryComponent from "./CountryComponent";
+import World from "./World";
+
+export {ZoomControls, Tooltip, CountryComponent, World};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+export { MapProvider } from './context/MapContext';
+export { default as useMap } from './hooks/useMap';
+// Export all components
+export * from './components';
+// Export types
+export * from './types';
