37c252acdd59fe35ed465cdaae8304306c14ccd9
Yanik <emmenegger@yanik.pro>
2024-12-11 10:18:31 +0100

    clean comments and cleanup

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,5 +1,3 @@
-// src/components/Tooltip.tsx
-
 import React from 'react';
 import {TooltipData} from '../types';
 
@@ -8,16 +6,18 @@ interface TooltipProps {
 }
 
 /**
- * Tooltip component that displays information about a country.
+ * Tooltip component:
+ * Displays a tooltip at a given position (x, y) with provided content.
  */
 const Tooltip: React.FC<TooltipProps> = ({ tooltipData }) => {
     const { x, y, content } = tooltipData;
 
+    // Tooltip positioning logic ensures it appears above and centered relative to the mouse.
     return (
         <div
             style={{
-                top: y-40,
-                left: x-40,
+                top: y - 40,
+                left: x - 40,
                 transform: 'translate(-50%, -100%)',
                 whiteSpace: 'nowrap',
                 zIndex: 1000,
--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,13 +1,13 @@
-// src/hooks/useMap.ts
-
-import { useContext } from 'react';
-import { MapContext } from '../context/MapContext';
-import { MapContextProps } from '../types';
+import {useContext} from 'react';
+import {MapContext} from '../context/MapContext';
+import {MapContextProps} from '../types';
 
 /**
  * Custom hook to access the MapContext.
- * @returns MapContextProps
- * @throws Error if used outside of MapProvider
+ * This hook provides consumers with direct access to the map state and utilities.
+ *
+ * @returns MapContextProps - The current context state and functions.
+ * @throws Error if used outside of MapProvider - Ensures that the hook is used correctly.
  */
 const useMap = (): MapContextProps => {
     const context = useContext(MapContext);
